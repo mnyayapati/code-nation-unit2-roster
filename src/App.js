17fa0d329ce1882e704e2ec7075c58b9d5b17e80
mnyayapati <mnyayapa@gmail.com>
2021-12-09 18:11:57 -0800

    Started roster project

--- src/App.js
+++ src/App.js
@@ -4,7 +4,49 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
     </div>
   );
 }
+
+let students = [
+  {
+    name: "Karin",
+    school: "UC Berkeley",
+    gradYear: 2017,
+    color: "sage",
+    music: "Country",
+    youtubeURL: "https://www.youtube.com/watch?v=kXj_1u7YlUY",
+    app: "Instagram",
+    place: "Beach"
+  },
+  {
+    name: "Ana",
+    school: "CSUEB",
+    gradYear: 2016,
+    color: "green",
+    music: "Reggea",
+    youtubeURL: "https://www.youtube.com/channel/UCKxbp_TrC0liu7tl0xsmdoA",
+    app: "Instagram",
+    place: "Beach"
+  },
+  {
+    name: "Joe",
+    school: "School of Schools",
+    gradYear: 2021,
+    color: "orange",
+    music: "Music",
+    youtubeURL: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    app: "",
+    place: "Beach"
+  },
+  {
+    name: "Jayden",
+    school: "Burton High School",
+    gradYear: 2023,
+    color: "blue",
+    music: "No Preference",
+    youtubeURL: "",
+    app: "Instagram",
+    place: "Beach"
+  }
+];
